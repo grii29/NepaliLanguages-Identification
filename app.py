6fc873e26659e8edc6ae8f4d77cd28df4259865c
Dell <Dell@MacBook-Air.local>
2026-02-21 22:30:08 +0000

    added metrics chart

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import LinearSVC
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import os
 
 app = Flask(__name__)
@@ -17,15 +17,16 @@ app = Flask(__name__)
 vectorizer = joblib.load("models/vectorizer.joblib")
 model_names = ["MultinomialNB", "kNN", "DecisionTree", "RandomForest", "SVM"]
 models = {name: joblib.load(f"models/{name}.joblib") for name in model_names}
-accuracy_dict = joblib.load("models/accuracy_dict.joblib")
+metrics_dict = joblib.load("models/metrics_dict.joblib")
 
 
-#Convert dict keys/values to Python lists for JS
-labels = list(accuracy_dict.keys())
-values = list(accuracy_dict.values())
+labels = list(metrics_dict.keys())
+accuracy_values = [metrics_dict[name]['accuracy'] for name in labels if 'accuracy' in metrics_dict[name]]
+f1_values = [metrics_dict[name]['f1_score'] for name in labels]
+precision_values = [metrics_dict[name]['precision'] for name in labels]
+recall_values = [metrics_dict[name]['recall'] for name in labels]
 
 #FLASK ROUTES
-
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
@@ -34,13 +35,22 @@ def index():
         model = models[model_choice]
         X_input_vec = vectorizer.transform([text_input])
         prediction = model.predict(X_input_vec)[0]
+        model_metrics = metrics_dict.get(model_choice, {})
+
         return render_template(
             "result.html",
             text=text_input,
             model=model_choice,
             prediction=prediction,
-            labels = labels,
-            values=values
+            accuracy=model_metrics.get("accuracy", 0),
+            precision=model_metrics.get("precision", 0),
+            recall=model_metrics.get("recall", 0),
+            f1_score=model_metrics.get("f1_score", 0),
+            labels=labels or [],
+            accuracy_values=accuracy_values or [],
+            f1_values=f1_values,
+            precision_values=precision_values,
+            recall_values=recall_values
             )
 
     return render_template("index.html", models=models.keys())
